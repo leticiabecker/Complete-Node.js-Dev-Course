186d431b205e5c039e9440cf30f44cac39b1558d
Leticia Becker <leticiabecker@gmail.com>
2016-12-28 17:05:37 -0500

    Getting Input From User
    Installed yargs@3.15.0 module

---
+++ password-manager/example-args.js
@@ -0,0 +1,35 @@
+var argv = require('yargs').argv;
+var command = argv._[0];
+
+console.log(argv);
+//argv is an object
+//_ is a property of argv
+//the _'s value is an array, that stores all the comands passed in
+
+/*
+if (argv._[0] === 'hello') {
+	console.log('Hello World!');
+}
+*/
+
+//If we run 'node example-args.js hello --name Leticia' in the command line, a new property called 'name' will be created, with the value 'Leticia'
+
+/*
+if (command === 'hello' && typeof argv.name !== 'undefined') {
+	console.log('Hello ' + argv.name + '!');
+} else if (command === 'hello') {
+	console.log('Hello world!');
+}
+*/
+
+
+//// Challenge
+//Add the lastname property
+
+if (command === 'hello' && typeof argv.name !== 'undefined' && typeof argv.lastname !== 'undefined') {
+	console.log('Hello ' + argv.name + ' ' + argv.lastname + '!');
+} else if (command === 'hello' && typeof argv.name !== 'undefined') {
+	console.log('Hello ' + argv.name + '!');
+} else if (command === 'hello') {
+	console.log('Hello world!');
+}
